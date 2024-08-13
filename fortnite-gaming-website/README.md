1a9cd88e9938478dbd5a538564bee533a2050586
Rad-spencer <159556435+Rad-spencer@users.noreply.github.com>
2024-08-13 10:31:51 +0000

    Website

---
+++ fortnite-gaming-website/README.md
@@ -0,0 +1,4 @@
+# Fortnite-gaming Website
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/RADSPENCER/pen/JjQOEEL](https://codepen.io/RADSPENCER/pen/JjQOEEL).
+
---
+++ fortnite-gaming-website/src/script.js
@@ -0,0 +1,6 @@
+// JavaScript functionality for the website can be added here
+// Example: Carousel interactivity, modals, or other effects
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Add any interactivity you want here
+});
